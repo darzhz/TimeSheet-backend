7c1f85011263db108db2dd8f8103439b11029f8e
midhuunn <midhunkappil9@gmail.com>
2024-08-05 15:13:19 +0530

    Backend for previous experience is done

---
+++ Models/PreviousExperience.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TimeSheet.Models;
+[Table("preexp")]
+public class PreviousExperience
+{
+    [Key]
+    [Column("id")]
+    public int? Id {get;set;}
+
+    [Required]
+    [NotNull]
+    [Column("compname")]
+    public string?CompanyName{get;set;}
+
+    [ForeignKey("Users")]        
+    [Column("userid")]
+    public int? Userid{get;set;}
+
+    [Required]
+    [NotNull]
+    [Column("designation")]
+    public string? Designation{get;set;}
+
+
+
+    [Required]
+    [NotNull]
+    [Column("statdate")]
+    public DateTime? StartDate{get;set;}
+
+    [Required]
+    [NotNull]
+    [Column("enddate")]
+    public DateTime?LastDate{get;set;}
+
+}
